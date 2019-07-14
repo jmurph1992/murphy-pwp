686a520a4ee2faec7ff75c718b31aa8461cbec1c
Justin Murphy <j.murphyjt@gmail.com>
2019-07-14 11:12:42 -0600

    Second commit adding audience to file

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -6,9 +6,18 @@
 	</head>
 	<body>
 
-		<h3>Purpose:</h3>
-		<p>The purpose of this personal webpage is to create awareness for and bring attention to the video gaming clan
-		that I am a part of.</p>
+		<div>
+
+			<h3>Purpose:</h3>
+
+			<p>The purpose of this personal webpage is to create awareness for and bring attention to the video gaming clan
+			that I am a part of.</p> <br>
+
+			<h3>Audience:</h3>
+
+			<p>The intended audience for this web page is anyone interested in membership in the clan.</p>
+
+		</div>
 
 	</body>
 </html>
