a7c9073a9cad561e8ba996473c15e4def9d4c5ea
Justin Murphy <j.murphyjt@gmail.com>
2019-07-14 11:08:36 -0600

    First commit with pwp purpose added

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # murphy-pwp
-repo for pwp project
+repo for pwp project <br> <br>
+This is a project that is a display page for the 
+gaming clan that I am a member of.
---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>i Feel Pretty</title>
+	</head>
+	<body>
+
+		<h3>Purpose:</h3>
+		<p>The purpose of this personal webpage is to create awareness for and bring attention to the video gaming clan
+		that I am a part of.</p>
+
+	</body>
+</html>
+
+
+<?php
