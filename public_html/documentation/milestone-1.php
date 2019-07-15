1dc821c43456de2a03715d17fc970ba2d921fc39
Justin Murphy <j.murphyjt@gmail.com>
2019-07-15 15:13:51 -0600

    Code formatted for easier reading.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -11,7 +11,7 @@
 			<h3>Purpose:</h3>
 
 			<p>The purpose of this personal webpage is to create awareness for and bring attention to the video gaming clan
-			that I am a part of.</p> <br>
+				that I am a part of.</p> <br>
 
 			<h3>Audience:</h3>
 
@@ -20,7 +20,7 @@
 			<h3>Goal:</h3>
 
 			<p>The goal of this web page is to make it easier for potential clan members to find the clan, express
-			interest in joining the clan, and to make it easier to get to know the clan members.</p>
+				interest in joining the clan, and to make it easier to get to know the clan members.</p>
 
 		</div>
 
@@ -28,7 +28,8 @@
 
 			<h3>Persona:</h3>
 
-			<img src="lonely-gamer-actual.jpg" width="700" height="525" alt="Lonely guy is missing" title="Our lonely gamer"/>
+			<img src="lonely-gamer-actual.jpg" width="700" height="525" alt="Lonely guy is missing"
+				  title="Our lonely gamer"/>
 
 			<ul>
 				<li><strong> Name: </strong> Gerald Gustafson</li>
@@ -38,12 +39,15 @@
 				<li><strong> Relevant technology: </strong> PlayStation 4, Xbox One, Gaming PC</li>
 				<li><strong> Behaviors: </strong> IRL friends do not play video games, plays alone, social person</li>
 				<li><strong> Scenario: </strong> Gerald will be using this site to get into contact with
-															other gamers that will help him out and game with him.</li>
+					other gamers that will help him out and game with him.
+				</li>
 				<li><strong> Goal: </strong> Gerald's goal for using this site is to find and enroll in a clan
-														that plays the same games he does so he will have friends to play with.</li>
+					that plays the same games he does so he will have friends to play with.
+				</li>
 				<li><strong> Frustrations: </strong> Gerald is frustrated that he has no online friends he can play
-																his favorite games with. Because of this, he is also locked out of
-																some endgame content that requires teams to get into.</li>
+					his favorite games with. Because of this, he is also locked out of
+					some endgame content that requires teams to get into.
+				</li>
 
 			</ul>
 
