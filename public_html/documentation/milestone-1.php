58493656e2abce93680fe0bc6599650babda75c7
Justin Murphy <j.murphyjt@gmail.com>
2019-07-15 14:55:15 -0600

    third commit adding the persona and a picture to the upload

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -15,7 +15,37 @@
 
 			<h3>Audience:</h3>
 
-			<p>The intended audience for this web page is anyone interested in membership in the clan.</p>
+			<p>The intended audience for this web page is anyone interested in membership in the clan.</p> <br>
+
+			<h3>Goal:</h3>
+
+			<p>The goal of this web page is to make it easier for potential clan members to find the clan, express
+			interest in joining the clan, and to make it easier to get to know the clan members.</p>
+
+		</div>
+
+		<div>
+
+			<h3>Persona:</h3>
+
+			<img src="lonely-gamer-actual.jpg" alt="Lonely guy is missing" title="Our lonely gamer"/>
+
+			<ul>
+				<li><strong> Name: </strong> Gerald Gustafson</li>
+				<li><strong> Age: </strong> 23</li>
+				<li><strong> Gender: </strong> Male</li>
+				<li><strong> Occupation: </strong> Retail sales</li>
+				<li><strong> Relevant technology: </strong> PlayStation 4, Xbox One, Gaming PC</li>
+				<li><strong> Behaviors: </strong> IRL friends do not play video games, plays alone, social person</li>
+				<li><strong> Scenario: </strong> Gerald will be using this site to get into contact with
+															other gamers that will help him out and game with him.</li>
+				<li><strong> Goal: </strong> Gerald's goal for using this site is to find and enroll in a clan
+														that plays the same games he does so he will have friends to play with.</li>
+				<li><strong> Frustrations: </strong> Gerald is frustrated that he has no online friends he can play
+																his favorite games with. Because of this, he is also locked out of
+																some endgame content that requires teams to get into.</li>
+
+			</ul>
 
 		</div>
 
