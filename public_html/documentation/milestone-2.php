ccc4d27ec277678d04820c5b5296bb6005d051c8
jmurph1992 <j.murphyjt@gmail.com>
2019-07-22 15:40:34 -0600

    Added phase 2 work: content strategy and wire frames.

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Phase 2a</title>
+	</head>
+	<body>
+
+		<h1>Content Strategy</h1>
+
+		<div>
+
+			<h3>Home Page</h3>
+
+			<p>This will be a one page site that contains the following sections: </p>
+
+			<p><strong>Welcome: </strong> This section of the site is at the top and is the first thing the visitor will see.
+				It will contain the name of the clan in large cursive letters as well as the clan motto. For the background, I
+				will place a photo or some sort of graphic that is related to the clan, nothing too attention grabbing.</p><br>
+
+			<p><strong>About us: </strong> This section will give an introduction to the clan. This will contain our mission
+				statement, our goal as a clan, and our general structure as a clan. This section will also include a list (or gallery)
+				of all the games we play as a clan. This list will not cover every game every member plays, but rather a collection of games
+				that we like to play as a group.</p> <br>
+
+			<p><strong>Portfolio: </strong> This part of the site will have an image gallery of the core members of the clan.
+				It will have some animation, I would like to make the member pictures clickable and have them flip over and
+				have limited member info on the other side. </p> <br>
+
+			<p><strong>Contact: </strong> This section will contain a simple contact form that sends emails/membership requests
+				to a selected email address. At the footer will be a link to the clan page for further information and resources.</p>
+		</div>
+
+		<div>
+			<h3>Full Screen Layout</h3>
+			<img src="desktop-ui-2.jpg" alt="full screen layout" title="Full screen layout design"/>
+			<h3>Mobile Layout</h3>
+			<img src="handheld-ui-2.jpg" alt="handheld layout" title="Mobile layout"/>
+		</div>
+	</body>
+</html>
+
+<?php
